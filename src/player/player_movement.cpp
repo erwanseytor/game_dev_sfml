35353f643e1a9c76c3988e3ca356e39d0f7d0693
erwan <erwan.seytor@epitech.eu>
2024-11-21 23:48:33 +0100

    fix [player_movement]: reduced the number of function calls

--- src/player/player_movement.cpp
+++ src/player/player_movement.cpp
@@ -15,16 +15,11 @@ using namespace std;
 
 void player_movement_handling(shared_ptr<OBJ> player_obj)
 {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-        sf::Vector2f pos = player_obj->get_position();
+    sf::Vector2f pos = player_obj->get_position();
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
         player_obj->set_position(pos.x + 0.1, pos.y);
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
-        sf::Vector2f pos = player_obj->get_position();
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
         player_obj->set_position(pos.x - 0.1, pos.y);
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-        sf::Vector2f pos = player_obj->get_position();
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
         player_obj->set_position(pos.x, pos.y + 0.1);
-    }
 }
